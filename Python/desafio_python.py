e975ada5da4e9d8bb80d71c80e49e47026eb2407
ryan <104521089+RyaanVictor@users.noreply.github.com>
2024-03-02 11:07:43 -0300

    Add files via upload

---
+++ Python/desafio_python.py
@@ -0,0 +1,7 @@
+#Escreva "Digite a nota 1"
+nome =input("Digite o seu Nome:")
+
+#Escreva "Digite a nota 2"
+data =input("Digite a sua Idade:" )
+
+print(f"Olá,{nome} vejo que você esta com {data} de idade")
